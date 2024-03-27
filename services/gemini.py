6da042f2fcbcef66cea821fed769eaa889c7f136
Micha Motyka <szortownikq@gmail.com>
2024-03-27 18:59:58 +0100

    make it better

--- services/gemini.py
+++ services/gemini.py
@@ -13,4 +13,4 @@ class GeminiService():
             response = model.generate_content(message)
             return response.text
         except Exception as e:
-            print(e)
+            self.generate_message(message=message)
